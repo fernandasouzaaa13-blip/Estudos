cb6d896a5d2b34d43b42be318ba162353fe3ca49
Maria Fernanda <fernandasouzaaa13@gmail.com>
2026-05-29 21:49:03 -0300

    Alterações

---
+++ Estudos JS/Leia/ex03.js
@@ -0,0 +1,13 @@
+const leia = require("prompt-sync")()
+let altura = leia("Digite sua altura: ")
+
+
+if (altura>=40){
+    console.log("Pode entrar")
+}
+if (if (altura >= 1.20 && altura <= 1.39) {
+  console.log("Pode entrar acompanhado")
+}
+if (altura < 1,20) {
+  console.log("Não pode entrar")
+  }
